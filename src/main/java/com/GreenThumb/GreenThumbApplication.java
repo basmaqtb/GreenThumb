1f53fe050897920e5f98791b73f7702c22cce012
basmaqtb <qotbibasma8@gmail.com>
2024-08-12 14:07:56 +0100

    First commit : création du projet Spring boot

---
+++ src/main/java/com/GreenThumb/GreenThumbApplication.java
@@ -0,0 +1,13 @@
+package com.GreenThumb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenThumbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenThumbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GreenThumb/GreenThumbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GreenThumb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenThumbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
